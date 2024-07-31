ff096ab5e51d71b4aa91e1fb8a1c5501a8a08f06
gyowon <81372512+gyowon@users.noreply.github.com>
2024-08-01 03:38:05 +0900

    [Silver V] Title: 최소공배수, Time: 116 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/13241. 최소공배수/최소공배수.py
+++ 백준/Silver/13241. 최소공배수/최소공배수.py
@@ -1,36 +1,14 @@
 import sys
 A, B=map(int, sys.stdin.readline().split())
-dic_a={}
-Test_a = A
-for a in range(2,A+1):
-    while Test_a%a==0:
-        if a in dic_a:
-            dic_a[a]+=1
-        else:
-            dic_a[a]=1
-        Test_a=Test_a/a
-dic_b={}
-Test_b = B
-for b in range(2,B+1):
-    while Test_b%b==0:
-        if b in dic_b:
-            dic_b[b] += 1
-        else:
-            dic_b[b] = 1
-        Test_b=Test_b/b
-arr=[]
-for i in dic_a:
-    if i in dic_b:
-        if dic_a[i] > dic_b[i]:
-            arr += [i] * dic_a[i]
-        else:
-            arr += [i] * dic_b[i]
-    else:
-        arr += [i] * dic_a[i]
-for i in dic_b:
-    if i not in dic_a:
-        arr += [i] * dic_b[i]
+if A>B:
+    a,b=A,B
+else:
+    a,b=B,A
 com=1
-for i in range(len(arr)):
-    com*=arr[i]
-print(com)
+for i in range(b,0,-1):
+    if a%i==0 and b%i==0:
+        com=i
+        break
+    else:
+        com-=1
+print(int(A*B/com))

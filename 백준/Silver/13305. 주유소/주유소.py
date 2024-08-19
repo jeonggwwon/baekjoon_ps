8c5cd13fb30359c2f59f9d829ebcb696c279714f
gyowon <81372512+gyowon@users.noreply.github.com>
2024-08-19 22:49:25 +0900

    [Silver III] Title: 주유소, Time: 120 ms, Memory: 133628 KB, Score: 17 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -0,0 +1,18 @@
+import sys
+N=int(sys.stdin.readline())
+city_len=list(map(int,sys.stdin.readline().split()))
+city_sum=0
+for i in range(N-1):
+    city_sum+=city_len[i]
+L_price=list(map(int,sys.stdin.readline().split()))
+min=L_price[0]*city_sum
+check=L_price[0]
+for i in range(1,N-1):
+    if L_price[1]<check:
+        city_sum-=city_len[0]
+        min-=check*city_sum
+        check=L_price[i]
+        min+=check*city_sum
+    else:
+        pass
+print(min)

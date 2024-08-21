ccd17e17dd0f9cf4d14be89125f09eb3b3e4a169
gyowon <81372512+gyowon@users.noreply.github.com>
2024-08-22 00:59:19 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,27 +1,17 @@
 import sys
 eq=sys.stdin.readline()
-eq_L=[]
 I=''
+sum, check=0,0
 for i in eq:
-    if i=='+' or i=='-':
-        eq_L+=[I]
-        I=''
-        eq_L+=i
-    elif i!='\n':
-        I+=i
-    if i=='\n':
-        eq_L += [I]
-sum=0
-check=0
-for i in eq_L:
-    if i=='+':
-        continue
-    elif i=='-':
-        check=1
-        continue
+    if i!='-' and i!='+' and i!='\n':
+        I += i
     else:
-        if check==0:
-            sum+=int(i)
-        elif check==1:
-            sum-=int(i)
+        if check == 1:
+            sum -= int(I)
+            I = ''
+        else:
+            sum += int(I)
+            I = ''
+        if i=='-':
+            check=1
 print(sum)

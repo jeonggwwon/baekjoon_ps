d67fd396b358323f9f5ba1e517ae58b5afe1f454
gyowon <81372512+gyowon@users.noreply.github.com>
2024-09-09 23:34:30 +0900

    [Silver I] Title: 곱셈, Time: 84 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -0,0 +1,14 @@
+import sys
+A,B,C=map(int,sys.stdin.readline().split())
+
+def multi(A,n):
+    if n==1:
+        return A%C
+    else:
+        tmp=multi(A,n//2)
+        if n%2==0:
+            return (tmp*tmp)%C
+        else:
+            return (tmp*tmp*A)%C
+
+print(multi(A,B))

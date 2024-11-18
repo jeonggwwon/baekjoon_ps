7ce4caa158cd692542164e71aff829a88862b159
gyowon <81372512+gyowon@users.noreply.github.com>
2024-11-19 01:47:50 +0900

    [Gold IV] Title: 이분 그래프, Time: 1136 ms, Memory: 639376 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,25 +1,26 @@
 import sys
-sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(21000)
 K=int(sys.stdin.readline())
 
-def DFS(start, mark):
-    visited[start]=mark
+def DFS(start):
+    global binary
     for next in graph[start]:
         if visited[next]==0:
-            if DFS(next,-mark):
-                pass
+            if visited[start]==1:
+                visited[next]=-1
             else:
-                return 0
+                visited[next]=1
+            DFS(next)
         elif visited[next]==visited[start]:
-            return 0
-    return 1
+            binary=0
+            return
+
 
 for k in range(K):
     V, E = map(int, sys.stdin.readline().split())
-    graph=[[] for _ in range(V+1)]
-    visited=[0]*(V+1)
+    graph = [[] for _ in range(V+1)]
+    visited = [0]*(V+1)
     binary=1
-
     for e in range(E):
         p1, p2 = map(int, sys.stdin.readline().split())
         graph[p1].append(p2)
@@ -27,10 +28,10 @@ for k in range(K):
 
     for v in range(1,V+1):
         if visited[v]==0:
-            binary=DFS(v,1)
+            visited[v]=1
+            DFS(v)
             if binary==0:
                 break
-
     if binary:
         print('YES')
     else:

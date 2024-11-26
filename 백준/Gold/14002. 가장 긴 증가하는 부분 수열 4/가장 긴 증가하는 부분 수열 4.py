6c23fae0927ab98d81892aab063d3e676c572a49
gyowon <81372512+gyowon@users.noreply.github.com>
2024-11-26 11:09:07 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 132 ms, Memory: 114000 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
@@ -3,10 +3,13 @@ from collections import deque
 N=int(sys.stdin.readline())
 A=list(map(int,sys.stdin.readline().split()))
 dp=[1]*N
-for i in range(N):
-    for j in range(i):
+for i in range(1,N):
+    temp=[]
+    for j in range(0,i):
         if A[j]<A[i]:
-            dp[i]=max(dp[i],dp[j]+1)
+            temp+=[dp[j]]
+    if len(temp)>0:
+        dp[i]+=max(temp)
 
 result=[0]*max(dp)
 for i in range(max(dp),0,-1):
@@ -17,5 +20,6 @@ for i in range(max(dp),0,-1):
         if dp[j]==i and A[j]<result[i]:
             result[i-1]=A[j]
             break
+
 print(max(dp))
 print(*result)
